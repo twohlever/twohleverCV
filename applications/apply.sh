2387cfc4d5fbff3f9c063be8cc7c3a7961c813f6
twohlever <theresa.wohlever@gmail.com>
2021-07-05 12:54:05 -0400

    Broad Institute Product Support Specialist - Application Submitted 2021-06-28

--- applications/apply.sh
+++ applications/apply.sh
@@ -1,2 +1,3 @@
-gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=Wohlever_Application.pdf coverLetter.pdf ../../wohlever_cv.pdf 
-
+pdflatex -interaction=nonstopmode ../../wohlever_cv.tex
+pdflatex -interaction=nonstopmode coverLetter.tex
+gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=Wohlever_Application.pdf coverLetter.pdf ../../wohlever_cv.pdf
