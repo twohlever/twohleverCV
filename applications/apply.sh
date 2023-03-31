6443deaa3f58e87bde5de270b3ad13ec197721a1
Theresa <twohlever@users.noreply.github.com>
2023-03-31 17:11:38 -0400

    fix relative path

--- applications/apply.sh
+++ applications/apply.sh
@@ -1,3 +1,6 @@
-pdflatex -interaction=nonstopmode ../../wohlever_cv.tex
+cd ../..
+pdflatex -interaction=nonstopmode wohlever_cv.tex
+# pdflatex -interaction=nonstopmode ../../wohlever_cv.tex
+cd -
 pdflatex -interaction=nonstopmode coverLetter.tex
 gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=Wohlever_Application.pdf coverLetter.pdf ../../wohlever_cv.pdf
