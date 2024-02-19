2783bd7221d501a888c1e18cca236e5d21fb633e
Theresa <twohlever@users.noreply.github.com>
2024-02-19 13:11:46 -0500

    2023 Applications

--- applications/apply.sh
+++ applications/apply.sh
@@ -1,6 +1,22 @@
+#!/bin/bash
+
+cvTex="wohlever_cv.tex"
+coverTex="coverLetter.tex"
+myDir="$(pwd)"
+
 cd ../..
-pdflatex -interaction=nonstopmode wohlever_cv.tex
-# pdflatex -interaction=nonstopmode ../../wohlever_cv.tex
-cd -
-pdflatex -interaction=nonstopmode coverLetter.tex
-gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=Wohlever_Application.pdf coverLetter.pdf ../../wohlever_cv.pdf
+
+if [ -s "${cvTex}" ]
+then
+    pdflatex -interaction=nonstopmode "${cvTex}"
+fi
+
+cd "${myDir}"
+
+if [ -s "${coverTex}" ]
+then
+    pdflatex -interaction=nonstopmode "${coverTex}"
+    gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=Wohlever_Application.pdf coverLetter.pdf ../../wohlever_cv.pdf
+else
+    gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=Wohlever_Application.pdf ../../wohlever_cv.pdf
+fi
